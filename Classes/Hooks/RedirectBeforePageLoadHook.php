562264ef58a45d56bdf928ecfcf5eade7b74cc4f
Aswathy S <aswathy.h@pitsolutions.com>
2019-05-24 17:57:18 +0530

    exception handling

--- Classes/Hooks/RedirectBeforePageLoadHook.php
+++ Classes/Hooks/RedirectBeforePageLoadHook.php
@@ -17,40 +17,48 @@ class RedirectBeforePageLoadHook{
      */
     public function execute(&$params, &$pagerenderer)
     {
+
         if (TYPO3_MODE !== 'FE') {
              return;
         }
-        $requestUrl = $_SERVER['HTTP_HOST'];
+        try{
+            $requestUrl = $_SERVER['HTTP_HOST'];
 
-        $objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\Extbase\\Object\\ObjectManager');
-        $configurationManager = $objectManager->get('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManager');
-        $extbaseFrameworkConfiguration = $configurationManager->getConfiguration(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+            $objectManager = GeneralUtility::makeInstance('TYPO3\\CMS\Extbase\\Object\\ObjectManager');
+            $configurationManager = $objectManager->get('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManager');
+            $extbaseFrameworkConfiguration = $configurationManager->getConfiguration(\TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
 
-        //get typoscript configuration for redirect rules
-        $redirectRule = $extbaseFrameworkConfiguration['plugin.']['tx_geoiphandler_geoiphandler.']['settings.']['redirects.'];
+            //get typoscript configuration for redirect rules
+            $redirectRule = $extbaseFrameworkConfiguration['plugin.']['tx_geoiphandler_geoiphandler.']['settings.']['redirects.'];
 
-        $extPath = ExtensionManagementUtility::extPath('geo_ip_handler');
-        $reader = new Reader($extPath.'Resources/Public/GeoLite/GeoLite2-City.mmdb');
-        $currentIp = GeneralUtility::getIndpEnv('REMOTE_ADDR');
-        $record = $reader->city($currentIp);
-        $isoCode = $record->country->isoCode ;
- 
-        //if iso code of clients's ip exists in redirect rule
-        if(array_key_exists(strtolower($isoCode).'.', $redirectRule)){
-            $target = $redirectRule[strtolower($isoCode).'.']['target'];
-            $trigger = $redirectRule[strtolower($isoCode).'.']['trigger'];
-            if( $trigger === 'in' ){
-                if( $requestUrl != $target ){
-                    HttpUtility::redirect($target);
+            $extPath = ExtensionManagementUtility::extPath('geo_ip_handler');
+            $reader = new Reader($extPath.'Resources/Public/GeoLite/GeoLite2-City.mmdb');
+            $currentIp = GeneralUtility::getIndpEnv('REMOTE_ADDR');
+            $record = $reader->city($currentIp);
+            $isoCode = $record->country->isoCode ;
+     
+            //if iso code of clients's ip exists in redirect rule
+            if(array_key_exists(strtolower($isoCode).'.', $redirectRule)){
+                $target = $redirectRule[strtolower($isoCode).'.']['target'];
+                $trigger = $redirectRule[strtolower($isoCode).'.']['trigger'];
+                if( $trigger === 'in' ){
+                    if( $requestUrl != $target ){
+                        HttpUtility::redirect($target);
+                    }
+                }
+                elseif($trigger === 'out'){
+                    $this->redirectToTarget($redirectRule, strtolower($isoCode), $requestUrl);
                 }
             }
-            elseif($trigger === 'out'){
+            else{
                 $this->redirectToTarget($redirectRule, strtolower($isoCode), $requestUrl);
             }
+
         }
-        else{
-            $this->redirectToTarget($redirectRule, strtolower($isoCode), $requestUrl);
+        catch(\Exception $e) {
+          echo $e->getMessage();
         }
+        
         return;
     }
 
